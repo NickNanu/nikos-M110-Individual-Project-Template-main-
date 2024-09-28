1b18f4f993a31e1793ccde58ce1e850202cb07cb
Noha Morte <pilgrimlight.olga@gmail.com>
2024-09-29 01:01:59 +0300

    Multiple changes.

---
+++ Assets/Reusable Assets/M110/Scripts/GameMechanics/Movement/Follow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts._Utils
+{
+    internal class Follow : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        private void Update()
+        {
+            this.transform.position = target.position;
+        }
+
+    }
+}
